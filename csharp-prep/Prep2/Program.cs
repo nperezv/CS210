df1c577a8782b4890474b72425046871bd18c429
nperezv <nelson.m.pvent93@gmail.com>
2024-04-29 01:37:12 +0200

    Prep1 & Prep2 Uploaded

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please type your grade percentage: ");
+        string UserInput = Console.ReadLine();
+        int percentage = int.Parse(UserInput);
+
+        string grade = "";
+
+        if(percentage >= 90)
+        {
+            grade = "A";
+        }
+
+        else if(percentage >= 80)
+        {
+            grade = "B";
+        }
+
+        else if(percentage >= 70)
+        {
+            grade = "C";
+        }
+
+        else if(percentage >= 60)
+        {
+            grade = "D";
+        }
+
+        else
+        {
+            grade = "F";
+        }
+        
+        Console.WriteLine($"Your final grade is: {grade}");
+
+        if(percentage >= 75)
+        {
+            Console.WriteLine("Passed!");
+        }
+
+        else
+        {
+            Console.WriteLine("Keep working hard. Try again!");
+        }
     }
 }
