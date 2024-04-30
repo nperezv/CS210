c37b19024aa89aec61b1797d85abe6bf455e5d76
nperezv <nelson.m.pvent93@gmail.com>
2024-04-30 03:31:55 +0200

    Prep3 file updated.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Magic Number provided by User input
+        // Console.Write("Please type a number to set the Magic Number: ");
+        // int MagicNumber = int.Parse(Console.ReadLine());
+        
+        // Random Magin Number configured
+        Random randomGenerator = new Random();
+        int MagicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        // We could also use a do-while loop here...
+        while (guess != MagicNumber)
+        {
+            Console.Write("Please type your guess: ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (MagicNumber > guess)
+            {
+                Console.WriteLine("Get higher");
+            }
+            else if (MagicNumber < guess)
+            {
+                Console.WriteLine("Get lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }                    
     }
 }
